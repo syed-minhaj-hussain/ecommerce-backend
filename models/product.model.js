b8cea88190032462ce6b3bc2c92f7aa188d6db23
SYED MINHAJ HUSSAIN <syed.minhaj0911@gmail.com>
2021-08-15 22:38:34 +0530

    refactor

--- models/product.model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const { Schema } = mongoose;
-
-const ProductSchema = new Schema({ name: String, price: Number });
-
-const Product = mongoose.model("Product", ProductSchema);
-
-module.exports = { Product };
