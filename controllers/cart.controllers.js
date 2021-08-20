4b99df028451f904dfeea39ad10c82af29ced6ce
SYED MINHAJ HUSSAIN <syed.minhaj0911@gmail.com>
2021-08-21 00:28:57 +0530

    refactor

--- controllers/cart.controllers.js
+++ controllers/cart.controllers.js
@@ -7,7 +7,7 @@ const getCartItemsController = async (req, res) => {
   try {
     const cartItems = await Cart.find({ user: _id });
 
-    res.status(200).json({ success: true, cartItems });
+    res.status(200).json(cartItems);
   } catch (err) {
     res.status(404).json({ success: false, message: "Cart Not Found" });
   }
